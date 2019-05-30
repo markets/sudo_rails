76b4eb1ed84b3831fc98ad17d7393c17be845561
Marc <srmarc.ai@gmail.com>
2019-05-30 22:12:26 +0200

    switch to sassc-rails (#5)

--- lib/sudo_rails.rb
+++ lib/sudo_rails.rb
@@ -2,6 +2,7 @@ require "sudo_rails/version"
 require "sudo_rails/controller_ext"
 require "sudo_rails/styling"
 require "sudo_rails/engine"
+require "sassc-rails"
 
 module SudoRails
   class << self
