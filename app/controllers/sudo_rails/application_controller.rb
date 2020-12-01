2e7b87f0762bdf3696f3dc946d305cb7456cafbd
Lautaro Castillo Barranco <lautarocastillo.93@gmail.com>
2020-12-01 16:45:08 +0100

    Add flash alert and support for custom styles

--- app/controllers/sudo_rails/application_controller.rb
+++ app/controllers/sudo_rails/application_controller.rb
@@ -5,9 +5,11 @@ module SudoRails
     def confirm
       if request.post? && SudoRails.confirm?(self, params[:password])
         session[:sudo_session] = Time.zone.now.to_s
+      else
+        flash[:alert] = I18n.t('sudo_rails.invalid_pass')
       end
 
-      redirect_to params[:target_path], alert: params[:alert]
+      redirect_to params[:target_path]
     end
 
     private
