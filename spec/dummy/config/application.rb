d8b26c5680d7a2c0be2985647268735c5b7330c3
Marc Anguera Insa <srmarc.ai@gmail.com>
2021-03-05 12:27:28 +0100

    fix CI: as we don't require all Rails frameworks in the dummy app, we need to ensure some ActiveSupport extensions are loaded

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,6 +4,7 @@ require 'action_controller/railtie'
 require 'action_view/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
+require 'active_support/all'
 
 Bundler.require(*Rails.groups)
 
@@ -11,9 +12,6 @@ require "sudo_rails"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
