8ca5d962b9198b5ea10ad78be971545847f51bed
Tejas <tejas-shetty@outlook.com>
2024-02-01 06:18:09 +0000

    Create a generator

---
+++ lib/generators/sudo_rails/view_generator.rb
@@ -0,0 +1,7 @@
+class SudoRails::ViewGenerator < Rails::Generators::Base
+    source_root File.expand_path("../../../app/views/sudo_rails/", __dir__)
+
+    def copy_initializer_file
+        copy_file "confirm_form.html.erb", "app/views/sudo_rails/confirm_form.html.erb"
+    end
+end
